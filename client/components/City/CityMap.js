ab4d82a6b07dcfc9a34757815f1573e363d6f1c7
Clément <clmntrss.pro@gmail.com>
2018-02-21 10:56:35 +0100

    push GoogleMap Component /!\ Not working

--- client/components/City/CityMap.js
+++ client/components/City/CityMap.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import ReactDOM from 'react-dom';
 
-export class GoogleMaps extends React.Component {
-    static defaultProps = {
-        center: {lat: 59.95, lng: 30.33},
-        zoom: 11
-    };
 
-    render() {
-        return (
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: [YOUR_KEY] }}
-                defaultCenter={this.props.center}
-                defaultZoom={this.props.zoom}
-            >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text={'Kreyser Avrora'}
-                />
-            </GoogleMapReact>
+var CityMap = React.createClass({
+    componentDidMount: function() {
+        var map;
+        var latLong = new google.maps.LatLng(-47.888723, 444.675360);
+        var options = {
+                zoom: 2,
+                center: latLong
+            },
+            map = new google.maps.Map(ReactDOM.findDOMNode(this), options);
+    },
+    render: function() {
+        return(
+            <div id="map"><span></span></div>
         );
     }
-}
+});
+
+
+
+export {CityMap};
