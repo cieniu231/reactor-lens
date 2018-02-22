b6bed79fe77534d7fb96dab26f7c4fe3c166ffa6
Dawid Sowa <dawidpawelsowa@gmail.com>
2018-02-22 22:45:19 +0100

    Final fixes

--- client/Home.js
+++ client/Home.js
@@ -6,7 +6,6 @@ import {Footer} from './components/Footer/Footer'
 import {Banner} from "./components/Banner/Banner";
 
 export default class Home extends React.Component {
-
     render() {
         return (
             <div>
@@ -19,3 +18,5 @@ export default class Home extends React.Component {
         );
     }
 }
+
+// final version
